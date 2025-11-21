9b3d8e628efd991af415fccaa6a55cc4c9d5fff3
sokolova_el <sokolova_el@taximaxim.ru>
2025-11-21 23:53:16 +0300

    lesson22

---
+++ src/main/kotlin/ru/kotiki/lessons/lesson22/homeworks/ExIllegalStateException.kt
@@ -0,0 +1,4 @@
+package org.example.ru.kotiki.lessons.lesson22.homeworks
+
+class ExIllegalStateException(message: String) : RuntimeException(message){
+}
---
+++ src/main/kotlin/ru/kotiki/lessons/lesson22/homeworks/ExNumberException.kt
@@ -0,0 +1,4 @@
+package org.example.ru.kotiki.lessons.lesson22.homeworks
+
+class ExNumberException(message: String) : RuntimeException(message) {
+}
